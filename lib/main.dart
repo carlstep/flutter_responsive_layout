8ee3af82681e77bc61b0cfead7c873c45325adf6
carlstep <carlstep333@me.com>
2022-08-11 10:08:27 +0700

    extract ListView to own widget - ListData

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,55 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Responsive Layout'),
+      ),
+      body: ListData(),
+    );
+  }
+}
+
+class ListData extends StatelessWidget {
+  const ListData({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.person),
+          trailing: Icon(Icons.waves),
+          title: Text('Person ${index + 1}'),
+        );
+      },
+    );
+  }
+}
