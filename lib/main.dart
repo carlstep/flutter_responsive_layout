50b29eda0cd1e991f2c44b401b16ca6d330f138a
carlstep <carlstep333@me.com>
2022-08-11 10:38:45 +0700

    using LayoutBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,23 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    Orientation orientation = MediaQuery.of(context).orientation;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Responsive Layout'),
-      ),
-      body: ListData(),
-    );
+        appBar: AppBar(
+          title: Text('Responsive Layout'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              print('Media: $height');
+              print('Builder: ${constraints.maxHeight}');
+              return ListData();
+            },
+          ),
+        ));
   }
 }
 
@@ -50,6 +61,27 @@ class ListData extends StatelessWidget {
           title: Text('Person ${index + 1}'),
         );
       },
+    );
+  }
+}
+
+class GridData extends StatelessWidget {
+  const GridData({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: 5,
+      children: List.generate(20, (index) {
+        return ListTile(
+          leading: Icon(Icons.person),
+          trailing: Icon(Icons.waves),
+          title: Text('Person ${index + 1}'),
+        );
+      }),
     );
   }
 }
